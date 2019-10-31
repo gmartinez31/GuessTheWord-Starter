a851d453c4bd94524a2ea5a0cb14a467a4bde0bd
gmartinez31 <bhgustavo13@yahoo.com>
2019-10-30 22:06:28 -0600

    show the timer

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,10 +1,12 @@
 package com.example.android.guesstheword.screens.game
 
 import android.os.CountDownTimer
+import android.text.format.DateUtils
 import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 public class GameViewModel : ViewModel() {
@@ -34,8 +36,10 @@ public class GameViewModel : ViewModel() {
         get() = _currentTime
 
     private val timer: CountDownTimer
-
     private lateinit var wordList: MutableList<String>
+    val currentTimeString = Transformations.map(currentTime) {
+        time -> DateUtils.formatElapsedTime(time)
+    }
 
     init {
         Log.i("GameViewModel", "GameViewModel created!")
